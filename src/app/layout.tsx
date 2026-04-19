3e264b07627a27034fa3c25568a66e39fa3caffe
Dimas Maulana Dwi Saputra <dimas@dicoding.com>
2026-04-19 03:46:20 +0000

    fix: suppress hydration warning on html/body for font loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,9 @@ export default function RootLayout({
     <html
       lang="id"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <AppShell>{children}</AppShell>
       </body>
     </html>
